7cdbbc6aa41ebd644522cb3ffe35838524dda1cb
Naga Sai Srinivas Mudunuri <mudunurinagasai222@gmail.com>
2026-09-15 12:10:27 +0530

    Added HTML Tag verification for Google Search Console

--- app/routes/pages.py
+++ app/routes/pages.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, make_response
 from app.models import Note
 from app import db
 
@@ -8,6 +8,33 @@ pages_bp = Blueprint('pages', __name__)
 @pages_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@pages_bp.route('/googleb06055eade7239f1.html')
+def google_verification():
+    return "google-site-verification: googleb06055eade7239f1.html"
+
+
+@pages_bp.route('/robots.txt')
+def robots():
+    response = make_response("User-agent: *\nAllow: /\nDisallow: /api/\nSitemap: https://codevault-m7er.onrender.com/sitemap.xml")
+    response.headers["Content-Type"] = "text/plain"
+    return response
+
+
+@pages_bp.route('/sitemap.xml')
+def sitemap():
+    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://codevault-m7er.onrender.com/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>"""
+    response = make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+    return response
 
 
 @pages_bp.route('/<note_id>')
